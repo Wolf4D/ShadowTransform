a216a9f059cd6bcec2d0cb4e89ef1a18dbbd429b
Ivan Klenov <Wolf4D@list.ru>
2017-12-14 17:51:25 +0300

    Добавлено сгорание падающих объектов

---
+++ Assets/ShadowTransform/Demo/Scripts/KeepParticlesAlive.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeepParticlesAlive : MonoBehaviour {
+	public float timer = 6.0f;
+	// Use this for initialization
+	void OnDestroy()
+	{
+		ParticleSystem[] part;
+		part = GetComponentsInChildren<ParticleSystem>();
+
+		foreach (ParticleSystem ps in part)
+		{
+			ps.transform.parent = null;
+			ps.gameObject.AddComponent<Rigidbody> ();
+			Destroy(ps.gameObject, timer);
+		}
+	}
+}
---
+++ Assets/ShadowTransform/Demo/Scripts/ObjectDestroyerZone.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDestroyerZone : MonoBehaviour {
+	public GameObject detonatorEffect;
+	public float timer = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider target)
+	{
+		if (target!=null)
+		if (!target.gameObject.isStatic) {
+			GameObject tmp = Instantiate (detonatorEffect, target.transform) as GameObject;
+			tmp.transform.position = target.transform.position;
+			tmp.transform.rotation = target.transform.rotation;
+
+			Destroy (target.transform.gameObject, timer);
+
+
+				
+		}
+		//Debug.Log (target);
+	}
+}
