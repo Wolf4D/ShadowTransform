ca2bf2d4f994dea403c424a9c9d691599703622a
Ivan Klenov <Wolf4D@list.ru>
2017-12-19 14:16:08 +0300

    Прокомментирован ShadowTransform

--- Assets/ShadowTransform/Scripts/ShadowTransform.cs
+++ Assets/ShadowTransform/Scripts/ShadowTransform.cs
@@ -1,63 +1,96 @@
-﻿using UnityEngine;
+﻿///////////////////////////////////////////////////////////////////////////////
+// ShadowTransform by Ivan Klenov (aka Wolf4D). 2017.
+//
+// All rights reserved.
+// Under Creative Commons Attribution (CC BY) License.
+// So, use it as you wish, just include me in credits.
+/////////////////////////////
+//
+// Main script for ShadowTransform. A heart of it all :)
+// Idea is simple too - just let's hold some structs with obj's parameters
+// for an each of states. If any parameter of obj differs from all of the saved
+// states - object is not in any of saved states. Else - if some state has that
+// values of pos, rot and scale - so, object is in that state :)
+//
+///////////////////////////////////////////////////////////////////////////////
+
+using UnityEngine;
 using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
 
+///////////////////////////////////////////////////////////////////////////////
+// Structure for holding a state's parameters
 
-[System.Serializable]
+[System.Serializable]  // else we could not save states in scenes
 public class ShadowState
 {
-    public string name = "";
-    public Vector3 position;
-    public Vector3 eulerAngles;
-    public Vector3 lossyScale;
+    public string name = "";    // name for state
+    public Vector3 position;    // absolute (world) pos of object in this state
+    public Vector3 eulerAngles; // absolute (world) rotation of object
+    public Vector3 lossyScale;  // absolute (world) scale of object
 };
 
-[DisallowMultipleComponent]
-public class ShadowTransform : MonoBehaviour {
+///////////////////////////////////////////////////////////////////////////////
 
+[DisallowMultipleComponent]
+public class ShadowTransform : MonoBehaviour
+{
+    // List of ShadowStates. It's called phantoms, cause we already have word
+    // "shadow" in every corner of our programm :)
     [SerializeField] public List<ShadowState> phantoms = new List<ShadowState>();
+
+    // TODO: make a shadow color adjustable sometimes somehow
     public Color shadowColor = new Color (0.6f, 0.15f, 0.7f, 0.4f);
 
-    // Use this for initialization
-    void Awake () {
-        if (this.gameObject.GetComponents<ShadowTransform> ().Length > 1) {
+/////////////////////////////
+
+    void Awake ()
+    {
+        if (this.gameObject.GetComponents<ShadowTransform> ().Length > 1)
+        {
             Debug.LogWarning ("This GameObject (" + this.gameObject.name +") already has one shadow transform!");
             Destroy (this);
         }
-        //(Material)Resources.Load("ShadowTransformMaterial");
-        //Destroy (this);		//На старте игры - отключаем все фантомы!!!
     }
 
+/////////////////////////////
+
+    // Add new phantom with current rot/pos/scale and defined name
     public void AddPhantom(string pname)
     {
-        ShadowState newShadow = new ShadowState ();
+        ShadowState newPhantom = new ShadowState ();
 
-        newShadow.name = pname; // + " (" + phantoms.Count + ")";
-        newShadow.position = this.gameObject.transform.position;
-        newShadow.eulerAngles = this.gameObject.transform.eulerAngles;
-        //Debug.Log (newShadow.eulerAngles);
+        newPhantom.name = pname; // + " (" + phantoms.Count + ")";
+        newPhantom.position = this.gameObject.transform.position;
+        newPhantom.eulerAngles = this.gameObject.transform.eulerAngles;
 
+        // Hack for calculating global scale - unparent an object,
+        // define it's scale, parent it back
         Transform cparent = this.gameObject.transform.parent;
         this.gameObject.transform.SetParent (null);
-        newShadow.lossyScale = this.gameObject.transform.lossyScale;
+        newPhantom.lossyScale = this.gameObject.transform.lossyScale;
         this.gameObject.transform.SetParent (cparent);
 
-        //добавить сюда гизмо
-        //скрыть дочерний фантом из дерева
-        phantoms.Add (newShadow);
+        // add new phantom to the phantom array
+        phantoms.Add (newPhantom);
 
-        // вызовем принудительно, чтобы отрисовать гизмо
+        // called manually to draw gizmo before first scene redraw
         EditorUtility.SetDirty(this);
 
     }
 
+/////////////////////////////
+
+    // Change object pos/rot/scale to some saved state by its id
     public void SwitchToPhantom(int id)
     {
-        if ((id >= 0) && (id < phantoms.Count)) {
+        if ((id >= 0) && (id < phantoms.Count))
+        {
             this.transform.position = phantoms [id].position;
             this.transform.rotation = Quaternion.Euler (phantoms [id].eulerAngles);
 
+            // Same parenting hack as before
             Transform cparent = this.transform.parent;
             this.transform.SetParent (null);
             this.transform.localScale = phantoms[id].lossyScale;
@@ -65,63 +98,95 @@ public class ShadowTransform : MonoBehaviour {
         }
     }
 
+/////////////////////////////
+
+    // Removes state from phantom array
     public void DeletePhantom()
     {
-        //удаляем фантом из массива
-        //переключаемся на другой фантом
-        //удаляем объект фантома
         phantoms.Remove(phantoms[CurrentPhantom()]);
-
         EditorUtility.SetDirty(this);
     }
 
+/////////////////////////////
+
+    // Finds what state has such a pos/rot/scale as object has
+    // at this moment, and returns phantom's id. Returns -1 if
+    // phantom wasn't found
     public int CurrentPhantom()
     {
         int current = -1;
         foreach (ShadowState obj in phantoms)
             if (obj!=null)
             {
-				if (VectorsAreEqual(obj.position, this.transform.position) &&
-					VectorsAreEqual(obj.eulerAngles, this.transform.eulerAngles) &&
-					VectorsAreEqual(obj.lossyScale, this.transform.lossyScale))
-                    current = phantoms.LastIndexOf (obj);
+                // Here you need to do a fuzzy compare of vectors.
+                // Standard procedure of comparing is not good, cause floating
+                // point numbers ars so floaty, and 1.0f may turn into
+                // 1.0000001 or 0.9999999. Unity makes some inside works, so
+                // you can compare vectors and get a good results... most of the time.
+                // But sometimes it does not work, so it needs to be fuzzy
+                if (VectorsAreEqual(obj.position, this.transform.position) &&
+                    VectorsAreEqual(obj.eulerAngles, this.transform.eulerAngles) &&
+                    VectorsAreEqual(obj.lossyScale, this.transform.lossyScale))
+                        current = phantoms.LastIndexOf (obj);
             }
 
         return current;
     }
 
+/////////////////////////////
 
+    // Draws all object shadows
     void OnDrawGizmos()
     {
         Gizmos.color = shadowColor;
-        //Handles.color = Color.white;
         MeshFilter meshFilter = this.GetComponent<MeshFilter> ();
-        foreach (ShadowState obj in phantoms) {
+
+        foreach (ShadowState obj in phantoms)
+        {
+            // We won't draw a mesh this for a static object during play - you
+            // just should not move static objects during runtime!
             if (!(this.gameObject.isStatic && Application.isPlaying))
             {
                 if (meshFilter!=null)
                     Gizmos.DrawMesh (meshFilter.sharedMesh, obj.position, Quaternion.Euler (obj.eulerAngles), obj.lossyScale);
                 else
-                    Debug.LogWarning ("This GameObject (" + this.gameObject.name +") has no MeshFilters!\nThis version won't draw nested meshes - no shadow objects will be drawn.");
+                    Debug.LogWarning ("This GameObject (" + this.gameObject.name +") has no MeshFilters!\n" +
+                    	"This version won't draw nested meshes - no shadow objects will be drawn.");
             }
 
-            //if (!Application.isPlaying)
-            //{
+            // Unity's behaviour for handles is kinda strange - handles are
+            // being drawn near the edge of the screen even when theirs
+            // "mounting point" is not visible. So, we need to "filter" handles
+            // to drop out must-be-invisible ones.
+
+            // Projecting "mounting point" to the screen
             Vector3 objInView = Camera.current.WorldToViewportPoint (obj.position);
-            if ((objInView.x>=0) && (objInView.x<=1) && (objInView.y>=0) && (objInView.y<=1) && (objInView.z>0))
-                Handles.Label (obj.position, "[" + this.gameObject.name + "]\n" + obj.name + " (" + phantoms.LastIndexOf(obj) + ")");
-            //}
-            //else
-            //	Handles.Label (obj.position, this.gameObject.name + "\n" + obj.name);
+
+            // Is point on screen?
+            if ((objInView.x>=0)
+                 && (objInView.x<=1)
+                 && (objInView.y>=0)
+                 && (objInView.y<=1)
+                 && (objInView.z>0))
+                    Handles.Label (obj.position, "[" + this.gameObject.name + "]\n" + obj.name + " (" + phantoms.LastIndexOf(obj) + ")");
+
             //Gizmos.DrawIcon (obj.position, "shadowtransformicon.png");
         }
     }
 
-	bool VectorsAreEqual(Vector3 one, Vector3 two)
-	{
-		return (Mathf.Approximately (one.x, two.x) &&
-				Mathf.Approximately (one.y, two.y) &&
-				Mathf.Approximately (one.z, two.z));
-	}
+/////////////////////////////
+
+    // A simple fuzzy compare
+    bool VectorsAreEqual(Vector3 one, Vector3 two)
+    {
+        return (Mathf.Approximately (one.x, two.x) &&
+                        Mathf.Approximately (one.y, two.y) &&
+                        Mathf.Approximately (one.z, two.z));
+    }
 
 }
+
+///////////////////////////////////////////////////////////////////////////////
+// If you need any help, wanna make a proposal, need some advice or
+// want to employ me, feel free to e-mail me: Wolf4D@list.ru
+///////////////////////////////////////////////////////////////////////////////
