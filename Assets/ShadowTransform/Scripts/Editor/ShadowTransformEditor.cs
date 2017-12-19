214cfac2c6faf09d4483d3fc418da5eba93d9490
Ivan Klenov <Wolf4D@list.ru>
2017-12-19 09:24:33 +0300

    Форматирование ShadowTransform и его Editor-а

--- Assets/ShadowTransform/Scripts/Editor/ShadowTransformEditor.cs
+++ Assets/ShadowTransform/Scripts/Editor/ShadowTransformEditor.cs
@@ -6,135 +6,122 @@ using UnityEditor;
 //[ExecuteInEditMode]
 public class ShadowTransformEditor : Editor {
 
-int oldShadow = -1;
+    int oldShadow = -1;
 
-int cselect = -1;
-string newPhantomName = "Base state";
-	[SerializeField]	ShadowTransform shadow;
+    int cselect = -1;
+    string newPhantomName = "Base state";
+    [SerializeField] ShadowTransform shadow;
 
-void Awake()
-{
-    shadow = (ShadowTransform)this.target;
-		cselect = shadow.CurrentPhantom ();
-}
-
-// Use this for initialization
-public override void OnInspectorGUI()
-{
-		if ((shadow.gameObject.isStatic && Application.isPlaying)) {
-			EditorGUILayout.HelpBox ("Sorry, operations with static objects are not avalible in play mode!", MessageType.Warning, true);
-			return;
-		}	
-		//if (Event.current.type == EventType.Layout)
-		//	return;
-
-    GUIStyle styleCenter = new GUIStyle (GUI.skin.label);
-    styleCenter.alignment = TextAnchor.MiddleCenter;
-
-
-
-	//	if (shadow.phantoms.Count > 0)
-	//		newPhantomName = "Alternative state";
-		
-   
-
-    cselect = shadow.CurrentPhantom ();
-    if 	(cselect<0)
-			newPhantomName = GUILayout.TextField(newPhantomName);
-
-
-
-    if (shadow.phantoms.Count > 0)
+    void Awake()
     {
-        string[] tst = new string[shadow.phantoms.Count];
-        for (int i = 0; i < shadow.phantoms.Count; i++)
-				tst [i] = shadow.phantoms [i].name + " (" + i + ")";
-
-        EditorGUILayout.LabelField ("State:", styleCenter);
-        cselect = EditorGUILayout.Popup ("", cselect, tst);
+        shadow = (ShadowTransform)this.target;
+        cselect = shadow.CurrentPhantom ();
     }
 
-		if (shadow.phantoms.Count ==0)
-			  EditorGUILayout.LabelField ("No saved shadow states", styleCenter);
+    // Use this for initialization
+    public override void OnInspectorGUI()
+    {
+        if ((shadow.gameObject.isStatic && Application.isPlaying)) {
+            EditorGUILayout.HelpBox ("Sorry, operations with static objects are not avalible in play mode!", MessageType.Warning, true);
+            return;
+        }
+        //if (Event.current.type == EventType.Layout)
+        //	return;
 
-			
-    EditorGUILayout.BeginHorizontal ();
+        GUIStyle styleCenter = new GUIStyle (GUI.skin.label);
+        styleCenter.alignment = TextAnchor.MiddleCenter;
 
-if (shadow.phantoms.Count > 0)
-    if (GUILayout.Button ("<<")) {
-        if (cselect <= 0)
-            cselect = shadow.phantoms.Count-1;
-        else
-            cselect--;
-    }
 
-	if 	(cselect<0)
-    if (GUILayout.Button ("+"))
-        shadow.AddPhantom(newPhantomName);
-   
-		//Color newShadowColor = new Color (0.6f, 0.15f, 0.7f, 0.4f);
-		//newShadowColor = EditorGUILayout.ColorField (newShadowColor);
 
-if (shadow.phantoms.Count > 0)
-if 	(cselect>=0)
-    if (GUILayout.Button ("-"))
-    {   
-			shadow.DeletePhantom ();
-			cselect--;
-		}
+        //	if (shadow.phantoms.Count > 0)
+        //		newPhantomName = "Alternative state";
 
-if (shadow.phantoms.Count > 0)			
-		if (GUILayout.Button (">>")) 
-		{
-			if (cselect >= shadow.phantoms.Count-1)
-				cselect = 0;
-			else
-				cselect++;
-		}
 
-    EditorGUILayout.EndHorizontal ();
 
-    if (cselect != oldShadow)
-        shadow.SwitchToPhantom (cselect);
+        cselect = shadow.CurrentPhantom ();
+        if 	(cselect<0)
+            newPhantomName = GUILayout.TextField(newPhantomName);
 
-    oldShadow = cselect;
 
-}
-// Update is called once per frame
-/*
-                void OnValidate () {
-                        Debug.Log ("535353");
 
+        if (shadow.phantoms.Count > 0)
+        {
+            string[] tst = new string[shadow.phantoms.Count];
+            for (int i = 0; i < shadow.phantoms.Count; i++)
+                tst [i] = shadow.phantoms [i].name + " (" + i + ")";
+
+            EditorGUILayout.LabelField ("State:", styleCenter);
+            cselect = EditorGUILayout.Popup ("", cselect, tst);
+        }
+
+        if (shadow.phantoms.Count ==0)
+            EditorGUILayout.LabelField ("No saved shadow states", styleCenter);
+
+
+        EditorGUILayout.BeginHorizontal ();
+
+        if (shadow.phantoms.Count > 0)
+            if (GUILayout.Button ("<<")) {
+                if (cselect <= 0)
+                    cselect = shadow.phantoms.Count-1;
+                else
+                    cselect--;
+            }
+
+        if 	(cselect<0)
+            if (GUILayout.Button ("+"))
+                shadow.AddPhantom(newPhantomName);
+
+        //Color newShadowColor = new Color (0.6f, 0.15f, 0.7f, 0.4f);
+        //newShadowColor = EditorGUILayout.ColorField (newShadowColor);
+
+        if (shadow.phantoms.Count > 0)
+            if 	(cselect>=0)
+                if (GUILayout.Button ("-"))
+                {
+                    shadow.DeletePhantom ();
+                    cselect--;
                 }
-        */
-void ButtonFunction1()
-{
-    Debug.Log ("2341");
-}
 
+        if (shadow.phantoms.Count > 0)
+            if (GUILayout.Button (">>"))
+            {
+                if (cselect >= shadow.phantoms.Count-1)
+                    cselect = 0;
+                else
+                    cselect++;
+            }
 
-	// Add a menu item called "Double Mass" to a Rigidbody's context menu.
-	[MenuItem("CONTEXT/Transform/Add Shadow Transform")]
-	static void AddShadowTransform(MenuCommand command)
-	{
-		Transform tmp = (Transform)command.context;
-		if (tmp != null) 
-		{
-			if (!tmp.gameObject.GetComponent<ShadowTransform> ()) {
-				//tmp.gameObject.AddComponent<PhantomTransform> ();
-				Undo.AddComponent<ShadowTransform> (tmp.gameObject);
-				ShadowTransform shadowtr = tmp.gameObject.GetComponent<ShadowTransform> ();
-				for(int i=0; i<tmp.gameObject.GetComponents<Component>().Length; i++)
-					UnityEditorInternal.ComponentUtility.MoveComponentUp(shadowtr);
-			}
-			else
-				Debug.LogWarning ("This GameObject (" + tmp.gameObject.name +") already has one shadow transform!");
-		}
-		/*
-		Rigidbody body = (Rigidbody)command.context;
-		body.mass = body.mass * 2;
-		Debug.Log("Doubled Rigidbody's Mass to " + body.mass + " from Context Menu.");
-		*/
-	}
+        EditorGUILayout.EndHorizontal ();
+
+        if (cselect != oldShadow)
+            shadow.SwitchToPhantom (cselect);
+
+        oldShadow = cselect;
+
+    }
+    // Add a menu item called "Double Mass" to a Rigidbody's context menu.
+    [MenuItem("CONTEXT/Transform/Add Shadow Transform")]
+    static void AddShadowTransform(MenuCommand command)
+    {
+        Transform tmp = (Transform)command.context;
+        if (tmp != null)
+        {
+            if (!tmp.gameObject.GetComponent<ShadowTransform> ()) {
+                //tmp.gameObject.AddComponent<PhantomTransform> ();
+                Undo.AddComponent<ShadowTransform> (tmp.gameObject);
+                ShadowTransform shadowtr = tmp.gameObject.GetComponent<ShadowTransform> ();
+                for(int i=0; i<tmp.gameObject.GetComponents<Component>().Length; i++)
+                    UnityEditorInternal.ComponentUtility.MoveComponentUp(shadowtr);
+            }
+            else
+                Debug.LogWarning ("This GameObject (" + tmp.gameObject.name +") already has one shadow transform!");
+        }
+        /*
+                Rigidbody body = (Rigidbody)command.context;
+                body.mass = body.mass * 2;
+                Debug.Log("Doubled Rigidbody's Mass to " + body.mass + " from Context Menu.");
+                */
+    }
 
 }
--- Assets/ShadowTransform/Scripts/ShadowTransform.cs
+++ Assets/ShadowTransform/Scripts/ShadowTransform.cs
@@ -3,117 +3,118 @@ using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
 
+
 [System.Serializable]
 public class ShadowState
 {
-	public string name = "";
-	public Vector3 position;
-	public Vector3 eulerAngles;
-	public Vector3 lossyScale;
+    public string name = "";
+    public Vector3 position;
+    public Vector3 eulerAngles;
+    public Vector3 lossyScale;
 };
 
 [DisallowMultipleComponent]
 public class ShadowTransform : MonoBehaviour {
 
-	[SerializeField] public List<ShadowState> phantoms = new List<ShadowState>();
-	public Color shadowColor = new Color (0.6f, 0.15f, 0.7f, 0.4f);
+    [SerializeField] public List<ShadowState> phantoms = new List<ShadowState>();
+    public Color shadowColor = new Color (0.6f, 0.15f, 0.7f, 0.4f);
 
-	// Use this for initialization
-	void Awake () {
-		if (this.gameObject.GetComponents<ShadowTransform> ().Length > 1) {
-			Debug.LogWarning ("This GameObject (" + this.gameObject.name +") already has one shadow transform!");
-			Destroy (this);
-		}
-		//(Material)Resources.Load("ShadowTransformMaterial");
-		//Destroy (this);		//На старте игры - отключаем все фантомы!!!
-	}
+    // Use this for initialization
+    void Awake () {
+        if (this.gameObject.GetComponents<ShadowTransform> ().Length > 1) {
+            Debug.LogWarning ("This GameObject (" + this.gameObject.name +") already has one shadow transform!");
+            Destroy (this);
+        }
+        //(Material)Resources.Load("ShadowTransformMaterial");
+        //Destroy (this);		//На старте игры - отключаем все фантомы!!!
+    }
 
-	public void AddPhantom(string pname)
-	{
-		ShadowState newShadow = new ShadowState ();
+    public void AddPhantom(string pname)
+    {
+        ShadowState newShadow = new ShadowState ();
 
-		newShadow.name = pname; // + " (" + phantoms.Count + ")";
-		newShadow.position = this.gameObject.transform.position;
-		newShadow.eulerAngles = this.gameObject.transform.eulerAngles;
-		//Debug.Log (newShadow.eulerAngles);
+        newShadow.name = pname; // + " (" + phantoms.Count + ")";
+        newShadow.position = this.gameObject.transform.position;
+        newShadow.eulerAngles = this.gameObject.transform.eulerAngles;
+        //Debug.Log (newShadow.eulerAngles);
 
-		Transform cparent = this.gameObject.transform.parent;
-		this.gameObject.transform.SetParent (null);
-		newShadow.lossyScale = this.gameObject.transform.lossyScale;
-		this.gameObject.transform.SetParent (cparent);
+        Transform cparent = this.gameObject.transform.parent;
+        this.gameObject.transform.SetParent (null);
+        newShadow.lossyScale = this.gameObject.transform.lossyScale;
+        this.gameObject.transform.SetParent (cparent);
 
-		//добавить сюда гизмо
-		//скрыть дочерний фантом из дерева
-		phantoms.Add (newShadow);
+        //добавить сюда гизмо
+        //скрыть дочерний фантом из дерева
+        phantoms.Add (newShadow);
 
-		// вызовем принудительно, чтобы отрисовать гизмо
-		EditorUtility.SetDirty(this);
+        // вызовем принудительно, чтобы отрисовать гизмо
+        EditorUtility.SetDirty(this);
 
-	}
+    }
 
-	public void SwitchToPhantom(int id)
-	{
-		if ((id >= 0) && (id < phantoms.Count)) {
-			this.transform.position = phantoms [id].position;
-			this.transform.rotation = Quaternion.Euler (phantoms [id].eulerAngles);
+    public void SwitchToPhantom(int id)
+    {
+        if ((id >= 0) && (id < phantoms.Count)) {
+            this.transform.position = phantoms [id].position;
+            this.transform.rotation = Quaternion.Euler (phantoms [id].eulerAngles);
 
-			Transform cparent = this.transform.parent;
-			this.transform.SetParent (null);
-			this.transform.localScale = phantoms[id].lossyScale;
-			this.gameObject.transform.SetParent (cparent);
-		}
-	}
+            Transform cparent = this.transform.parent;
+            this.transform.SetParent (null);
+            this.transform.localScale = phantoms[id].lossyScale;
+            this.gameObject.transform.SetParent (cparent);
+        }
+    }
 
-	public void DeletePhantom()
-	{
-		//удаляем фантом из массива
-		//переключаемся на другой фантом
-		//удаляем объект фантома
-		phantoms.Remove(phantoms[CurrentPhantom()]);
+    public void DeletePhantom()
+    {
+        //удаляем фантом из массива
+        //переключаемся на другой фантом
+        //удаляем объект фантома
+        phantoms.Remove(phantoms[CurrentPhantom()]);
 
-		EditorUtility.SetDirty(this);
-	}
+        EditorUtility.SetDirty(this);
+    }
 
-	public int CurrentPhantom()
-	{
-		int current = -1;
-			foreach (ShadowState obj in phantoms) 
-			if (obj!=null)
-			{
-				if ((obj.position == this.transform.position) &&
-				    (obj.eulerAngles == this.transform.eulerAngles) &&
-				    (obj.lossyScale == this.transform.lossyScale))
-					current = phantoms.LastIndexOf (obj);
-			}
-			
-		return current;
-	}
+    public int CurrentPhantom()
+    {
+        int current = -1;
+        foreach (ShadowState obj in phantoms)
+            if (obj!=null)
+            {
+                if ((obj.position == this.transform.position) &&
+                        (obj.eulerAngles == this.transform.eulerAngles) &&
+                        (obj.lossyScale == this.transform.lossyScale))
+                    current = phantoms.LastIndexOf (obj);
+            }
+
+        return current;
+    }
 
 
-	void OnDrawGizmos()
-	{
-		Gizmos.color = shadowColor;
-		//Handles.color = Color.white;
-		MeshFilter meshFilter = this.GetComponent<MeshFilter> ();
-		foreach (ShadowState obj in phantoms) {
-			if (!(this.gameObject.isStatic && Application.isPlaying))
-			{
-			if (meshFilter!=null)
-				Gizmos.DrawMesh (meshFilter.sharedMesh, obj.position, Quaternion.Euler (obj.eulerAngles), obj.lossyScale);
-			else
-				Debug.LogWarning ("This GameObject (" + this.gameObject.name +") has no MeshFilters!\nThis version won't draw nested meshes - no shadow objects will be drawn.");
-			}
+    void OnDrawGizmos()
+    {
+        Gizmos.color = shadowColor;
+        //Handles.color = Color.white;
+        MeshFilter meshFilter = this.GetComponent<MeshFilter> ();
+        foreach (ShadowState obj in phantoms) {
+            if (!(this.gameObject.isStatic && Application.isPlaying))
+            {
+                if (meshFilter!=null)
+                    Gizmos.DrawMesh (meshFilter.sharedMesh, obj.position, Quaternion.Euler (obj.eulerAngles), obj.lossyScale);
+                else
+                    Debug.LogWarning ("This GameObject (" + this.gameObject.name +") has no MeshFilters!\nThis version won't draw nested meshes - no shadow objects will be drawn.");
+            }
 
-			//if (!Application.isPlaying)
-			//{
-			Vector3 objInView = Camera.current.WorldToViewportPoint (obj.position);
-			if ((objInView.x>=0) && (objInView.x<=1) && (objInView.y>=0) && (objInView.y<=1) && (objInView.z>0))
-				Handles.Label (obj.position, "[" + this.gameObject.name + "]\n" + obj.name + " (" + phantoms.LastIndexOf(obj) + ")");
-			//}
-			//else
-			//	Handles.Label (obj.position, this.gameObject.name + "\n" + obj.name);
-			//Gizmos.DrawIcon (obj.position, "shadowtransformicon.png");
-		}
-	}
-		
+            //if (!Application.isPlaying)
+            //{
+            Vector3 objInView = Camera.current.WorldToViewportPoint (obj.position);
+            if ((objInView.x>=0) && (objInView.x<=1) && (objInView.y>=0) && (objInView.y<=1) && (objInView.z>0))
+                Handles.Label (obj.position, "[" + this.gameObject.name + "]\n" + obj.name + " (" + phantoms.LastIndexOf(obj) + ")");
+            //}
+            //else
+            //	Handles.Label (obj.position, this.gameObject.name + "\n" + obj.name);
+            //Gizmos.DrawIcon (obj.position, "shadowtransformicon.png");
+        }
+    }
+
 }
